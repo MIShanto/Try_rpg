68087a9dc6a93752e27306fce6b1ffb373c18f73
Monirul Islam <shanto4045@gmail.com>
2020-07-18 12:46:21 +0600

    npc added

---
+++ Path/Assets/Scripts/NPC/NpcMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcMovement : MonoBehaviour
+{
+    public float speed;
+    float waitTime;
+    public float startWaitTime;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    public Transform moveSpot;
+    Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+        animator.SetFloat("Speed", 1f);
+        Vector2 direction = moveSpot.position - transform.position;
+        direction.Normalize();
+        if (direction != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", direction.x);
+            animator.SetFloat("Vertical", direction.y);
+        }
+
+        if (Vector2.Distance(transform.position, moveSpot.position) <= 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+
+                moveSpot.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+                animator.SetFloat("Speed", 0f);
+            }
+            }
+    }
+}
